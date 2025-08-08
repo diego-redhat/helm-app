4aa34ef209b04d5506e6e94789fcfe9e0b28fc07
Diego Oliveira <diego.dso123@gmail.com>
2025-08-08 14:32:53 -0300

    new helm version

--- render.sh
+++ render.sh
@@ -4,6 +4,10 @@ cp -pr templates/ temp/helm-demo/templates
 helm package temp/helm-demo --destination repo/
 helm repo index repo/
 rm -rf temp/
+echo "update git repo"
 git add .
 git commit -m "new helm version"
 git push origin main
+echo "update helm repo"
+cd repo
+helm push app-0.1.0.tgz oci://quay.io/doliveira1277/helm
